e9a1934b3d571d2a664be182cf1de004a4e3b815
Vincent Delestre <vdelestre02@gmail.com>
2023-04-05 12:54:55 +0200

    save mais rien ne fonctionne

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
 
+/*
 var bouton = document.createElement("BUTTON");
 var text = document.createTextNode("Connexion");
 bouton.appendChild(text);
 document.body.appendChild(bouton);
+*/
+
+/*var connexionBouton = function(){
+    var linkbutton = document.getElementsByClassName('Connexion');
+    
+    href ='login.html';
+}*/
